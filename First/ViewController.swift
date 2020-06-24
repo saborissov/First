5fed7b940a39a945a70eba58119d2d4dc578a0cd
17586317 <Borisov.S.Alek@sberbank.ru>
2020-06-24 22:03:54 +0300

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by 17586317 on 24.06.2020.
+//  Copyright © 2020 Борисов Сергей Александрович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
